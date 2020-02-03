811cd15ca62f11e9949ca2daf529634cf852fd84
BMeeNA57 <59247187+BMeeNA57@users.noreply.github.com>
2020-02-03 13:21:58 -0700

    Add files via upload

---
+++ Basic JavaScript Projects/Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,17 @@
+window.alert("Hello, World!");
+document.write("Hello, World! ");
+var A= "This is a String";
+document.write(A);
+var B= "This is another string. "
+        +" This is a concatenated string on a variable.";
+window.alert(B);
+document.write(" \"Hey guy's!\", said someone.");
+document.write(" Adding another string,"
++" a Concatenating string."); 
+var Family = "One Piece", captain ="Luffy", FirstMate = "Zorro", Cook = "Sanji", ThirdMate = "Nami";
+document.write(ThirdMate)
+var A = A.fontcolor("green");
+var B = B.fontcolor("red");
+document.write(A);
+document.write(B);
+document.write(5+5);
---
+++ Basic JavaScript Projects/Basic JavaScript Projects/JS/Basic_JavaScript_2.js
@@ -0,0 +1,13 @@
+var x = multiply(10, 5);
+document.getElementById("multiply").innerHTML = x;
+
+function multiply(a, b) {
+  return a * b;
+}
+
+
+function myFunction() {
+    var sentence="I am learning";
+    sentence += " to add a concatenate string.";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
