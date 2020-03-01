919a579c8e12619419bdb1c29ffd1555ae05cc51
BMeeNA57 <59247187+BMeeNA57@users.noreply.github.com>
2020-03-01 15:42:43 -0700

    Add files via upload

---
+++ Ajax/ajax/ajax.js
@@ -0,0 +1,22 @@
+var ajax = new XMLHttpRequest();
+ajax.open('GET','content.html');
+ajax.onreadystatechange = function () {
+    if (ajax.readyState = 4) {
+        document.getElementById('content').innerHTML = ajax.responseText;
+    }
+}
+function getContent() {
+    ajax.send ();
+    document.getElementById('btn_content').style.display = 'none';
+}
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("ajax").innerHTML =
+        this.responseText;
+      }
+    };
+    xhttp.open("GET", "./ajax_info.html", true);
+    xhttp.send();
+  }
